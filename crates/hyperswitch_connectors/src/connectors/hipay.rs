0dc7ce7e39c66dbe9905fb2c77987dcefeb1498b
adityagarud <garudaditya079@gmail.com>
2025-10-05 16:55:17 +0530

    feat(connector): [HIPAY] add integrity check support for Authorize, PSync, Refund & RSync flows

--- crates/hyperswitch_connectors/src/connectors/hipay.rs
+++ crates/hyperswitch_connectors/src/connectors/hipay.rs
@@ -332,12 +332,31 @@ impl ConnectorIntegration<Authorize, PaymentsAuthorizeData, PaymentsResponseData
             .response
             .parse_struct("Hipay PaymentsAuthorizeResponse")
             .change_context(errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        let amount = utils::convert_amount(
+            self.amount_converter,
+            data.request.minor_amount,
+            data.request.currency,
+        )?;
+
+        let response_integrity_object = utils::get_authorise_integrity_object(
+            self.amount_converter,
+            amount,
+            data.request.currency.to_string(),
+        )?;
+
         event_builder.map(|i| i.set_response_body(&response));
         router_env::logger::info!(connector_response=?response);
-        RouterData::try_from(ResponseRouterData {
+
+        let new_router_data = RouterData::try_from(ResponseRouterData {
             response,
             data: data.clone(),
             http_code: res.status_code,
+        });
+
+        new_router_data.map(|mut router_data| {
+            router_data.request.integrity_object = Some(response_integrity_object);
+            router_data
         })
     }
 
@@ -401,12 +420,31 @@ impl ConnectorIntegration<PSync, PaymentsSyncData, PaymentsResponseData> for Hip
             .response
             .parse_struct("hipay HipaySyncResponse")
             .change_context(errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        let amount = utils::convert_amount(
+            self.amount_converter,
+            data.request.minor_amount,
+            data.request.currency,
+        )?;
+
+        let response_integrity_object = utils::get_sync_integrity_object(
+            self.amount_converter,
+            amount,
+            data.request.currency.to_string(),
+        )?;
+
         event_builder.map(|i| i.set_response_body(&response));
         router_env::logger::info!(connector_response=?response);
-        RouterData::try_from(ResponseRouterData {
+
+        let new_router_data = RouterData::try_from(ResponseRouterData {
             response,
             data: data.clone(),
             http_code: res.status_code,
+        });
+
+        new_router_data.map(|mut router_data| {
+            router_data.request.integrity_object = Some(response_integrity_object);
+            router_data
         })
     }
 
@@ -643,12 +681,31 @@ impl ConnectorIntegration<Execute, RefundsData, RefundsResponseData> for Hipay {
             .response
             .parse_struct("hipay RefundResponse")
             .change_context(errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        let refund_amount = utils::convert_amount(
+            self.amount_converter,
+            data.request.minor_refund_amount,
+            data.request.currency,
+        )?;
+
+        let response_integrity_object = utils::get_refund_integrity_object(
+            self.amount_converter,
+            refund_amount,
+            data.request.currency.to_string(),
+        )?;
+
         event_builder.map(|i| i.set_response_body(&response));
         router_env::logger::info!(connector_response=?response);
-        RouterData::try_from(ResponseRouterData {
+
+        let new_router_data = RouterData::try_from(ResponseRouterData {
             response,
             data: data.clone(),
             http_code: res.status_code,
+        });
+
+        new_router_data.map(|mut router_data| {
+            router_data.request.integrity_object = Some(response_integrity_object);
+            router_data
         })
     }
 
@@ -711,12 +768,31 @@ impl ConnectorIntegration<RSync, RefundsData, RefundsResponseData> for Hipay {
             .response
             .parse_struct("hipay RefundResponse")
             .change_context(errors::ConnectorError::ResponseDeserializationFailed)?;
+
+        let refund_amount = utils::convert_amount(
+            self.amount_converter,
+            data.request.minor_refund_amount,
+            data.request.currency,
+        )?;
+
+        let response_integrity_object = utils::get_refund_integrity_object(
+            self.amount_converter,
+            refund_amount,
+            data.request.currency.to_string(),
+        )?;
+
         event_builder.map(|i| i.set_response_body(&response));
         router_env::logger::info!(connector_response=?response);
-        RouterData::try_from(ResponseRouterData {
+
+        let new_router_data = RouterData::try_from(ResponseRouterData {
             response,
             data: data.clone(),
             http_code: res.status_code,
+        });
+
+        new_router_data.map(|mut router_data| {
+            router_data.request.integrity_object = Some(response_integrity_object);
+            router_data
         })
     }
 
